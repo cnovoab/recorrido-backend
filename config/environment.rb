b76b7fd37581a887c658671752464f841ac8d8ee
Claudio Novoa <claudio.novoa@frogmi.com>
2019-03-17 13:12:41 -0300

    Fix migration + Sendgrid credential as ENV vars

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'cnovoab',
-  :password => 'ezln-1986',
-  :domain => 'recorrido.cl',
+  :user_name => ENV.fetch('SG_USER'),
+  :password => ENV.fetch('SG_PASS'),
+  :domain => ENV.fetch('SG_HOST'),
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 2019_03_16_101445) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "username"
+    t.string "last_name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
