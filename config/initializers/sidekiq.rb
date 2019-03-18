3800ae14e39aca72e26af283a956f59ce3402e59
Claudio Novoa <claudio.novoa@frogmi.com>
2019-03-18 07:17:03 -0300

    Sidekiq configuration for heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,8 @@
-sidekiq_config = { url: 'redis://127.0.0.1:6379' }
+if Rails.env.production?
+  sidekiq_config = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: 2 }
+else
+  sidekiq_config = { url: 'redis://127.0.0.1:6379' }
+end
 
 Sidekiq.configure_server do |config|
     config.redis = sidekiq_config
