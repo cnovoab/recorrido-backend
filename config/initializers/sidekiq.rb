c585584cec4b40a6aeea710b5408fef2958df613
Claudio Novoa <claudio.novoa@frogmi.com>
2019-03-18 07:56:24 -0300

    Fix sidekiq threads size

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,20 @@
+
 if Rails.env.production?
-  sidekiq_config = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'] }
+  Sidekiq.configure_server do |config|
+      config.redis = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: 4 }
+  end
+
+  Sidekiq.configure_client do |config|
+      config.redis = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: 2 }
+  end
 else
   sidekiq_config = { url: 'redis://127.0.0.1:6379' }
-end
+  Sidekiq.configure_server do |config|
+      config.redis = sidekiq_config
+  end
 
-Sidekiq.configure_server do |config|
-    config.redis = sidekiq_config.merge(size: 4)
-end
+  Sidekiq.configure_client do |config|
+      config.redis = sidekiq_config
+  end
 
-Sidekiq.configure_client do |config|
-    config.redis = sidekiq_config.merge(size: 2)
 end
