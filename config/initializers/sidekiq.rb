c47a5b90d355d0a3181ac06b18e18a487bfb0170
Claudio Novoa <claudio.novoa@frogmi.com>
2019-03-18 07:23:37 -0300

    Increment sidekiq workers

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
 if Rails.env.production?
-  sidekiq_config = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'], size: 2 }
+  sidekiq_config = { url: ENV['REDISTOGO_URL'] || ENV['REDIS_URL'] }
 else
   sidekiq_config = { url: 'redis://127.0.0.1:6379' }
 end
 
 Sidekiq.configure_server do |config|
-    config.redis = sidekiq_config
+    config.redis = sidekiq_config.merge(size: 4)
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = sidekiq_config
+    config.redis = sidekiq_config.merge(size: 2)
 end
