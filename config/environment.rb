84283f6f0607bd6914028c03efb5f99c1e449cfe
Claudio Novoa <claudio.novoa@inzpiral.com>
2019-03-22 05:59:26 -0300

    Set default values for sendgrid credentials

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV.fetch('SG_USER'),
-  :password => ENV.fetch('SG_PASS'),
-  :domain => ENV.fetch('SG_HOST'),
+  :user_name => ENV.fetch('SG_USER', 'SENDGRID_USER'),
+  :password => ENV.fetch('SG_PASS', 'SENDGRID_PASS'),
+  :domain => ENV.fetch('SG_HOST', 'SENDGRID_HOST'),
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
